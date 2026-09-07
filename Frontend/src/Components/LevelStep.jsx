9d7ad0e8e224bd14681d54ef0974af812b3801d5
Shemuel Castro <156734018+Shemuel-bot@users.noreply.github.com>
2026-09-07 03:54:31 +0000

    Refactor LevelStep component layout and styles; add level label and update donate button hover effects

--- Frontend/src/Components/LevelStep.jsx
+++ Frontend/src/Components/LevelStep.jsx
@@ -9,16 +9,22 @@ export default function LevelStep({ level, isActive, isCompleted, onClick }) {
   };
 
   return (
-    <>
-    <div className={styles.shadow}>
-      <div
-        className={`${styles.levelStep} ${isActive ? styles.active : ''} ${isCompleted ? styles.completed : ''}`}
-        onClick={handleClick}
-      >
-        <img src="https://img.icons8.com/?size=100&id=60003&format=png&color=FFFFFF" alt="lesson" className={styles.starIcon} />
+    <div className={styles.levelStepContainer}>
+      <div className={styles.shadow}>
+        <div
+          className={`${styles.levelStep} ${isActive ? styles.active : ''} ${isCompleted ? styles.completed : ''}`}
+          onClick={handleClick}
+        >
+          <img src="https://img.icons8.com/?size=100&id=60003&format=png&color=FFFFFF" alt="lesson" className={styles.starIcon} />
+        </div>
+        
       </div>
-      
+      <div className={styles.levelLabel}>
+        <h3 className={styles.levelText}>Level {level}</h3>
+        <button className={styles.levelButton} onClick={handleClick} disabled={!isActive && !isCompleted}>
+          Start Lesson
+        </button>
+      </div>
     </div>
-    </>
   );
 }
