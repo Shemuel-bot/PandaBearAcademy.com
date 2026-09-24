91740bc4cd1901c1aae03d4e6eba6f9c18ee6780
Shemuel Castro <156734018+Shemuel-bot@users.noreply.github.com>
2026-09-24 07:08:37 +0000

    I did some stuff in the backend

--- Backend/controllers/User.js
+++ Backend/controllers/User.js
@@ -7,3 +7,31 @@ export const getAllUsers = asyncHandler(async (req, res) => {
     const users = await db.orm.public.User.select("id", "email", "name").all();
     res.json(users);
 })
+
+export const userPost = [
+    body("email", "email must not be empty").trim().isLength({ min: 2}).escape,
+    body("username", "username needs to be filled").trim().isLength({min: 2}),
+    body("name", "give me your real name.").trim().isLength({min:2}),
+    body("password", "password must be nice").trim().isLength({min:2}).escape,
+    asyncHandler(async (req, res) => {
+        const error = validationResult(req);
+        const password = await bcrypt
+            .hash(req.body.password, 10)
+            .then(hash => hash)
+            .catch(err => console.log(err));
+        if(!error.isEmpty()){
+            res.json({
+                message: error.array()
+            })
+        }else{
+            await db.orm.public.User.create({
+                email: req.body.email,
+                password: password,
+                username: req.body.username,
+                name: req.body.name,
+
+            })
+        }
+    })
+
+]
--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,15 @@
 import express from "express";
 const router = express.Router();
-const UserController = require("../controllers/User.js");
+import UserController from './controllers/User.js'
 
 const app = express();
 app.use(express.json());
 
-app.use("/users", router);
+app.use("/", router);
 
-router.get("/", UserController.getAllUsers);
+router.get("/users/v1", UserController.getAllUsers);
+
+router.post("/users/v1/post", UserController.userPost)
 
 
 
